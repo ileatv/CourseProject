2b6d502a2fbf723d1286c2f20366d431e2002663
Ilya Barabanov <perfectiongeroi2@gmail.com>
2022-05-18 04:15:06 +0300

    Fixed a problem with image paths in the file scss.js

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,20 +1,34 @@
-// Проверка поддержки webp, добавление класса webp или no-webp для HTML
-export function isWebp(){
-    function testWebP(callback) {
-    // Проверка поддержки webp
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    //Добавление класса _webp или _no-webp для html
-    testWebP(function (support) {
+// // Проверка поддержки webp, добавление класса webp или no-webp для HTML
+// export function isWebp(){
+//     function testWebP(callback) {
+//     // Проверка поддержки webp
+//     var webP = new Image();
+//     webP.onload = webP.onerror = function () {
+//     callback(webP.height == 2);
+//     };
+//     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+//     }
+//     //Добавление класса _webp или _no-webp для html
+//     testWebP(function (support) {
     
-    if (support == true) {
-    document.querySelector('body').classList.add('webp');
-    }else{
-    document.querySelector('body').classList.add('no-webp');
+//     if (support == true) {
+//     document.querySelector('body').classList.add('webp');
+//     }else{
+//     document.querySelector('body').classList.add('no-webp');
+//     }
+//     });
+// }
+export function isWebp() {
+    function testWebP(callback) {
+        var webP = new Image();
+        webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+        };
+        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
     }
+
+    testWebP(function (support) {
+        let className = support === true ? 'webp' : 'no-webp'
+        document.documentElement.classList.add(className)
     });
 }
